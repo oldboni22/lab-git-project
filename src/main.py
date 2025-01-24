b0738a034ce75f1bb320572cf965eb69e105d59e
oldboni22 <oldboni22@gmail.com>
2025-01-24 15:48:00 +0300

    edit main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-#
+from random import randint as ri
+from utils import add_numbers, multiply_numbers
+
+def call_functions():
+    a = ri(0, 100)
+    b = ri(0, 100)
+    print(f"a = {a} and b = {b}")
+    print("a + b = {}".format(add_numbers(a, b)))
+    print("a * b = {}".format(multiply_numbers(a, b)))
+
+if __name__ == "__main__":
+    call_functions()
